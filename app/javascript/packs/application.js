f4a3c285f13133aead428eea227eb8ee12d5f4a9
EC2 Default User <ec2-user@ip-172-31-8-70.ap-northeast-1.compute.internal>
2023-11-29 18:42:22 +0000

    ユーザー詳細 レイアウト変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,3 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
-//= require jquery
-//= require bootstrap
-//= require_tree.
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -17,24 +9,24 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application";
 
-const dropdown = new bootstrap.Dropdown(element, {
-  popperConfig(defaultBsPopperConfig) {
-    // const newPopperConfig = {...}
-    // use defaultBsPopperConfig if needed...
-    // return newPopperConfig
-  }
-})
+      document.addEventListener("turbolinks:load", function() {
+        const input = document.querySelector('#post_image');
+        const preview = document.querySelector('#image-preview');
 
-// Turbolinksの読み込み完了後に初期化を行う
-document.addEventListener("turbolinks:load", function() {
-  // BootstrapのJavaScriptを再初期化
-  var navbarToggler = document.querySelector('.navbar-toggler');
-  navbarToggler.addEventListener('click', function() {
-    var target = document.querySelector(navbarToggler.dataset.bsTarget);
-    bootstrap.Collapse.getInstance(target).toggle();
-  });
-});
+        input.addEventListener('change', function(event) {
+          const file = event.target.files[0];
+          if (file) {
+            const reader = new FileReader();
 
+            reader.onload = function(e) {
+              preview.src = e.target.result;
+              preview.style.display = 'block';
+            };
+
+            reader.readAsDataURL(file);
+          }
+        });
+      });
 
 Rails.start()
 Turbolinks.start()

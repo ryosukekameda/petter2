102c1307b29e8e1dcdc4b2a213a3d6f3f048f331
EC2 Default User <ec2-user@ip-172-31-8-70.ap-northeast-1.compute.internal>
2023-12-17 12:21:17 +0000

    jpg

--- app/models/post.rb
+++ app/models/post.rb
@@ -68,7 +68,7 @@ class Post < ApplicationRecord
   
   def get_post_image(width, height)
     unless post_image.attached?
-      file_path = Rails.root.join('app/assets/images/1980x1080_image3.jpg')
+      file_path = Rails.root.join('app/assets/images/1980x1080_image.jpg')
       post_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
     end
       post_image.variant(resize_to_limit: [width, height]).processed
